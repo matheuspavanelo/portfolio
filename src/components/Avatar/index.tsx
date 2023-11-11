6c60e5d1a5051828e4a660ea9adc6ba28f17dcef
Matheus Pavanelo <matheus@exemplo.com>
2023-11-11 18:41:34 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/matheuspavanelo.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/matheuspavanelo.png" />
-    <Titulo fontSize={20}>Matheus Pavanelo</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Matheus Pavanelo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        matheuspavanelo
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor full-stack Python
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
